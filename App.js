e195a413d5ca550f45408e0a0c491ca0109d1ec3
PhilaNgcamu <philangcamu17@gmail.com>
2024-08-16 09:53:22 +0200

    Map views

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
-import React from "react";
-import { Provider } from "react-redux";
-import StackApp from "./src/screens/NavigationStack";
-import store from "./src/redux/store";
+import React, { useEffect, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View, Text } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
   return (
-    <Provider store={store}>
-      <StackApp />
-    </Provider>
+    <View style={styles.container}>
+      <MapView style={styles.map} region={region}>
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title={"You are here"}
+          />
+        )}
+      </MapView>
+      {errorMsg && <Text style={styles.errorText}>{errorMsg}</Text>}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  errorText: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    color: "red",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import qrCodeReducer from "./reducer";
-
-const store = createStore(qrCodeReducer);
-
-export default store;
